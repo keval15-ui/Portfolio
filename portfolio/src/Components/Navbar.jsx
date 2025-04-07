6e857752c97710dea2cde2a649165abdc597307d
keval solankure <kevalsolankure@gmail.com>
2025-04-07 12:54:23 +0530

    Making vercea

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "@/styles/Navbar.css";
+import "../styles/Navbar.css"; // Use relative path for CSS
 
 const Navbar = () => {
   return (
