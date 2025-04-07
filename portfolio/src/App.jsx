05c02e0b94f20d633b9756f1aca2ac413d591959
keval solankure <kevalsolankure@gmail.com>
2025-04-07 14:04:36 +0530

    Add Navbar component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx"; // Use relative path instead of alias
+import Navbar from "./pages/Navbar.jsx"; // Use relative path instead of alias
 import Homepage from "./pages/Homepage.jsx";
 import About from "./pages/About.jsx";
 import Projects from "./pages/Projects.jsx";
 import Contact from "./pages/Contact.jsx";
-import Footer from "./components/Footer.jsx";
+import Footer from "./pages/Footer.jsx";
 import "./App.css"; // Use relative path for CSS too
 
 function App() {
